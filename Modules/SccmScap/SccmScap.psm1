64c486b06877cf0ec3f506e7ebfbf69e0adcbaa6
Clive Northey <clnorthe@microsoft.com>
2020-03-03 09:45:45 -0500

    updates to SccmScap Module

--- Modules/SccmScap/SccmScap.psm1
+++ Modules/SccmScap/SccmScap.psm1
@@ -287,3 +287,44 @@ function New-ConfigurationFolders
     Move-CMObject -FolderPath $FolderPath -InputObject (Get-CMDeviceCollection -Name $Collection2.Name)
     Move-CMObject -FolderPath $FolderPath -InputObject (Get-CMDeviceCollection -Name $Collection3.Name)
 }
+
+<#
+.Synopsis
+   Import Scap configuration items and baselines in System Center Configuration Manager
+.DESCRIPTION
+   This function imports Scap configuration items and baselines in System Center
+   Configuration Manager
+.EXAMPLE
+    $ImportFolder = 'C:\temp\SCAP2Convert'
+    Import-SCAPBaseline -Path "$ImportFolder\Windows10"
+    Import-SCAPBaseline -Path "$ImportFolder\Server2016"
+    Import-SCAPBaseline -Path "$ImportFolder\Server2019"
+#>
+function Import-ScapBaseline
+{
+    [cmdletbinding()]
+    param
+    (
+        [Parameter(Mandatory = $true)]
+        [string]
+        $Path
+    )
+
+    $files = Get-ChildItem -Path $Path -File -Recurse -Include *.Cab
+
+    foreach ($file in $files)
+    {
+        Import-CMBaseline -FileName $file.FullName -Force
+        Write-Verbose -Verbose "$($file.Name) SCAP Baselines and Configuration Items imported successfully."
+    }
+}
+
+<#
+#Location for SCAP CAB Benchmarks
+$ImportFolder = 'C:\temp\SCAP2Convert'
+
+Import-SCAPBaseline -Path "$ImportFolder\Windows10"
+Import-SCAPBaseline -Path "$ImportFolder\Server2016"
+Import-SCAPBaseline -Path "$ImportFolder\Server2019"
+#>
+
